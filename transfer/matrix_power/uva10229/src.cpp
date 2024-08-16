9081ed77c569fa887ef3c727d8a36590a8c6c499
FouTY <g251055@dac.unicamp.br>
2024-08-16 11:47:12 -0300

    tentando resolver uva10229

--- transfer/matrix_power/uva10229/src.cpp
+++ transfer/matrix_power/uva10229/src.cpp
@@ -1,29 +1,26 @@
 #include <bits/stdc++.h>
+using namespace std;
 long long MOD;
-
-int mod(long long a, long long m){ return ((a % m) + m) % m; } // ensures mod will be positive.
 
 const int MAX_N = 2;
 struct Matrix{
   long long mat[MAX_N][MAX_N]; 
-  int row=MAX_N, col=MAX_N;
 };
 
 Matrix matMul(Matrix a, Matrix b){
   Matrix ans;
-  for( int i{0}; i < ans.row; ++i ){
-    for( int j{0}; j < ans.col; ++j ){
+  for( int i{0}; i < MAX_N; ++i ){
+    for( int j{0}; j < MAX_N; ++j ){
       ans.mat[i][j] = 0;
     }
   }
 
-  int dim = MAX_N;
-  for(int i{0}; i < dim; ++i ){
-    for(int k{0}; k < dim; ++k ){
+  for(int i{0}; i < MAX_N; ++i ){
+    for(int k{0}; k < MAX_N; ++k ){
       if( a.mat[i][k] == 0 ) continue;
-      for(int j{0}; j < dim; ++j ){
-        ans.mat[i][j] += mod(a.mat[i][k], MOD) * mod(b.mat[k][j], MOD);
-        ans.mat[i][j] = mod(ans.mat[i][j], MOD);
+      for(int j{0}; j < MAX_N; ++j ){
+        ans.mat[i][j] += (a.mat[i][k] % MOD) * (b.mat[k][j] % MOD);
+        ans.mat[i][j] = (ans.mat[i][j] % MOD);
       }
     }
   }
@@ -33,8 +30,8 @@ Matrix matMul(Matrix a, Matrix b){
 
 Matrix matPow( Matrix base, int p ){
   Matrix ans;
-  for(int i{0}; i < ans.row; ++i )
-    for(int j{0}; j < ans.col; ++j )
+  for(int i{0}; i < MAX_N; ++i )
+    for(int j{0}; j < MAX_N; ++j )
       ans.mat[i][j] = ( i == j ); // Identity Matrix
   
   while( p ){
@@ -47,22 +44,13 @@ Matrix matPow( Matrix base, int p ){
   return ans;
 }
 
-long long pow(int a, int p){
-  if( p == 0 ) return 1;
-  int ans = pow(a, p/2);
-  ans = ans * ans;
-  if( p & 1 )
-    ans = ans*a;
-
-  return ans;
-}
 
 int main(){
   long long a, b;
   Matrix fibMatrix;
   while( scanf("%lld %lld", &a, &b) != EOF ){
     fibMatrix.mat[0][0] = 1; fibMatrix.mat[0][1] = 1; fibMatrix.mat[1][0] = 1; fibMatrix.mat[1][1] = 0;
-    MOD = pow(2, b);
+    MOD = 1 << b;
     fibMatrix = matPow(fibMatrix, a - 1);
     printf("%lld\n", fibMatrix.mat[0][0]); 
   }
