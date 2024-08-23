75023f4f38f7f28feddcdeb5cceb690888e0364f
FouTY <g251055@dac.unicamp.br>
2024-08-23 13:58:20 -0300

    tentando fazer exercicio uva

--- transfer/big_integer/uva10183/src.py
+++ transfer/big_integer/uva10183/src.py
@@ -4,6 +4,21 @@ while(True):
     b = int(enters[1])
     if( a == 0 and b == 0):
         break
-    print(a)
-    print(b)
-    
+    counter = 0
+    fib_1 = 1
+    fib_2 = 2
+
+    if(a == fib_1 or a == fib_2):
+        counter += 1
+    if(b == fib_1 or b == fib_2):
+        counter += 1
+
+    while(True):
+        if(fib_2 > b):
+            break
+        if(fib_2 > a and fib_2 < b):
+            counter += 1
+        copy_fib = fib_2
+        fib_2 = fib_1 + fib_2
+        fib_1 = copy_fib
+    print(counter)
